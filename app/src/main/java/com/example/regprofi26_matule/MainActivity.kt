ecf905fcf8687caebfc9a2259a58021881882aed
frens <liadovn@yandex.ru>
2026-08-12 18:32:37 +0300

    create notification system

--- app/src/main/java/com/example/regprofi26_matule/MainActivity.kt
+++ app/src/main/java/com/example/regprofi26_matule/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.regprofi26_matule
 
+import android.Manifest
+import android.app.AlarmManager
+import android.app.AlarmManager.*
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,11 +23,13 @@ import com.example.netlibrary.network.NetworkMonitor
 import com.example.regprofi26_matule.DI.networkModule
 import com.example.regprofi26_matule.Domain.UserRepository
 import com.example.regprofi26_matule.Presentation.Navigation.Navigation
+import com.example.regprofi26_matule.Presentation.Notification.NotificationReceiver
 import com.example.regprofi26_matule.ui.theme.RegProfi26_MatuleTheme
 import com.example.uikit.UI.MatuleTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
 
@@ -40,11 +49,86 @@ class MainActivity : ComponentActivity() {
         UserRepository.init(this)
 
         enableEdgeToEdge()
+
+        if (
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                100
+            )
+        }
+
         setContent {
             MatuleTheme {
                 Navigation(isOnline.value)
             }
         }
+    }
+
+    private fun scheduleNotification() {
+
+        val alarmManager =
+            getSystemService(ALARM_SERVICE) as AlarmManager
+
+        val intent = Intent(
+            this,
+            NotificationReceiver::class.java
+        )
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            100,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+        )
+
+        alarmManager.setAndAllowWhileIdle(
+            RTC_WAKEUP,
+            System.currentTimeMillis() + 3 * 60 *1000,
+            pendingIntent
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (
+            !isChangingConfigurations &&
+            UserRepository.Notification
+        ) {
+            scheduleNotification()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        cancelNotification()
+    }
+
+    private fun cancelNotification() {
+
+        val alarmManager =
+            getSystemService(ALARM_SERVICE) as AlarmManager
+
+        val intent = Intent(
+            this,
+            NotificationReceiver::class.java
+        )
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            100,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+        )
+
+        alarmManager.cancel(pendingIntent)
     }
 }
 
---
+++ app/src/main/java/com/example/regprofi26_matule/Presentation/Notification/NotificationReceiver.kt
@@ -0,0 +1,39 @@
+package com.example.regprofi26_matule.Presentation.Notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.uikit.R
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val manager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager
+
+        val channel = NotificationChannel(
+            "return_channel",
+            "Напоминания",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        manager.createNotificationChannel(channel)
+
+        val notification = NotificationCompat.Builder(
+            context,
+            "return_channel"
+        )
+            .setSmallIcon(R.drawable.shopping_cart)
+            .setContentTitle("Matule")
+            .setContentText("Возвращайтесь скорее!")
+            .setAutoCancel(true)
+            .build()
+
+        manager.notify(1, notification)
+    }
+}
